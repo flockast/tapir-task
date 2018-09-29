47a766a894b444a75c066bb26196d6490a238128
konstantin <flockast@gmail.com>
2018-09-30 02:42:19 +0300

    desktop

--- webpack.config.js
+++ webpack.config.js
@@ -92,6 +92,17 @@ module.exports = {
                 options: {
                     name: '[path][name].[ext]'
                 }
+            },
+
+            // FONTS
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[path][name].[ext]'
+                    }
+                }]
             }
         ]
     }
